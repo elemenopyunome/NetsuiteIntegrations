ead259f7c57db32f4b3129d44745f0b1da171466
elemenopy <elemenopy@DESKTOP-IRVHDKF>
2020-02-21 08:18:27 -0600

    Add project files.

---
+++ NetsuiteIntegrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetsuiteIntegrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
